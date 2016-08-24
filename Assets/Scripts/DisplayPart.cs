e954888057fdc1a70b510123887e99e361cbd4b0
Haje <kaist.haje@gmail.com>
2016-08-24 22:03:36 +0900

    This Project is dead

---
+++ Assets/Scripts/DisplayPart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayPart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnMouseDown()
+    {
+        //Debug.Log("ok");
+        //GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<MoveitMoveit>().Addpart(PartManager.getPart(name));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
